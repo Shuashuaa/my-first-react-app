fcc04f32af816793d674d24b474faa42eb62e9c2
Shuashuaa <jovijoshuatania027@gmail.com>
2025-02-23 21:09:18 +0800

    inputs, labels and loading

--- react-crud-ts/src/components/ProductForm.tsx
+++ react-crud-ts/src/components/ProductForm.tsx
@@ -1,4 +1,7 @@
 import { Button } from "@/components/ui/button"
+import { Label } from "@/components/ui/label"
+import { Input } from "@/components/ui/input"
+import { Loader2 } from "lucide-react";
 
 interface ProductFormProps {
     sampleProductName: string;
@@ -9,6 +12,7 @@ interface ProductFormProps {
     setSampleProductPrice: React.Dispatch<React.SetStateAction<string>>;
     handleSubmit: (e: React.FormEvent) => void;
     editProductId: string | null;
+    loading: boolean;
 }
   
 const ProductForm: React.FC<ProductFormProps> = ({
@@ -19,36 +23,39 @@ const ProductForm: React.FC<ProductFormProps> = ({
     setSampleProductName,
     setSampleProductPrice,
     handleSubmit,
-    editProductId
+    editProductId,
+    loading
 }) => {
+
     return (
       <div>
         <h1 className='text-2xl mb-2'>{editProductId ? 'Edit Product' : 'Add a New Product'}</h1>
         <form className='flex flex-col gap-1 text-left' onSubmit={handleSubmit}>
-          <label>Enter a Product Name</label>
-          <input
-            value={sampleProductName} 
-            onChange={(e) => setSampleProductName(e.target.value)}
-            className="shadow appearance-none border border-gray-400 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
-            placeholder="Name" 
-          />
-          {formNameResult && <p className="text-red-400 text-sm">{formNameResult}</p>}
-          
-          <label>Enter a Product Price</label>
-          <input 
-            value={sampleProductPrice}
-            onChange={(e) => setSampleProductPrice(e.target.value)}
-            className="shadow appearance-none border border-gray-400 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
-            placeholder="Price" 
-          />
-          {formPriceResult && <p className="text-red-400 text-sm">{formPriceResult}</p>}
-          
-          {/* <button className='border border-gray-400 rounded-lg my-2 h-11 shadow'>
-            {editProductId ? 'Update' : 'Add'} Product
-          </button> */}
-          <Button className="my-2">
-            {editProductId ? 'Update' : 'Add'} Product
-          </Button>
+            <Label className="text-sm font-bold">Enter a Product Name</Label>
+            <Input
+                value={sampleProductName} 
+                onChange={(e) => setSampleProductName(e.target.value)}
+                className="w-[350px] shadow appearance-none border border-gray-400 rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
+                placeholder="Name" 
+            />
+            {formNameResult && <p className="text-red-400 text-sm">{formNameResult}</p>}
+            
+            <Label className="text-sm font-bold">Enter a Product Price</Label>
+            <Input 
+                value={sampleProductPrice}
+                onChange={(e) => setSampleProductPrice(e.target.value)}
+                className="w-[350px] shadow appearance-none border border-gray-400 rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
+                placeholder="Price" 
+            />
+            {formPriceResult && <p className="text-red-400 text-sm">{formPriceResult}</p>}
+            <Button className="w-[350px] my-2" disabled={loading}>
+                {loading ? (
+                <>
+                    <Loader2 className="animate-spin mr-2" />
+                    Loading...
+                </>
+                ) : ( editProductId ? 'Update' : 'Add' + ' Product' )}
+            </Button>
         </form>
       </div>
     );
