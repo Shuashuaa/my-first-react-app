728e194f4ceb976d1b7e3f9d2e5b2c2639b6c501
Shuashuaa <jovijoshuatania027@gmail.com>
2025-03-02 00:33:39 +0800

    previous and next buttons are removed for mobile and implemented a bigger button size instead

--- src/components/ui/pagination.tsx
+++ src/components/ui/pagination.tsx
@@ -61,7 +61,6 @@ PaginationLink.displayName = "PaginationLink"
 
 const PaginationPrevious = ({
   className,
-  title,
   ...props
 }: React.ComponentProps<typeof PaginationLink>) => (
   <PaginationLink
@@ -71,14 +70,13 @@ const PaginationPrevious = ({
     {...props}
   >
     <ChevronLeft className="h-4 w-4" />
-    <span>{title}</span>
+    <span>Previous</span>
   </PaginationLink>
 )
 PaginationPrevious.displayName = "PaginationPrevious"
 
 const PaginationNext = ({
   className,
-  title,
   ...props
 }: React.ComponentProps<typeof PaginationLink>) => (
   <PaginationLink
@@ -87,7 +85,7 @@ const PaginationNext = ({
     className={cn("gap-1 pr-2.5", className)}
     {...props}
   >
-    <span>{title}</span>
+    <span>Next</span>
     <ChevronRight className="h-4 w-4" />
   </PaginationLink>
 )
