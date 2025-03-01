89c03b5a48e633aae54995e200fda062d2eef059
Shuashuaa <jovijoshuatania027@gmail.com>
2025-03-02 00:32:57 +0800

    previous and next buttons are removed for mobile and implemented a bigger button size instead

--- src/components/PaginationSection.tsx
+++ src/components/PaginationSection.tsx
@@ -53,20 +53,19 @@ const PaginationSection: React.FC<PaginationProps> = ({
 
     const displayedPages = pages.slice(pageStart, pageEnd);
 
-    // mobile responsive for "Previous" and "Next" Buttons
-    const [prev, setPrev] = useState('Previous');
-    const [next, setNext] = useState('Next');
+    const [resp, setResp] = useState('pc');
+    const [buttonSize, setButtonSize] = useState('p-5');
 
     // FIX MY PAGINATION SECTION FOR RESPONSIIIIIIVEVEVEVE
 
     useEffect(() => {
         const handleResize = () => {
             if (window.innerWidth >= 1024) {
-                setPrev('Previous');
-                setNext('Next');
+                setResp('pc');
+                setButtonSize('p-5')
             } else {
-                setPrev('P');
-                setNext('N');
+                setResp('mobile');
+                setButtonSize('p-6')
             }
         };
 
@@ -81,29 +80,17 @@ const PaginationSection: React.FC<PaginationProps> = ({
     return (
         <Pagination>
             <PaginationContent>
-                <PaginationItem className={currentPage != 1 ? 'cursor-pointer' : 'cursor-default'}>
-                    <PaginationPrevious title={prev} onClick={handlePrevPage} className={currentPage === 1 ? "bg-neutral-200 rounded-md" : ""} />
-                </PaginationItem>
-
-                {/* {pages.map((page, idx) => (
-                <PaginationItem key={idx} className={currentPage === page ? "bg-neutral-200 rounded-md cursor-default" : "cursor-pointer"}>
-                    <PaginationLink onClick={() => setCurrentPage(page)}>{page}</PaginationLink>
-                </PaginationItem>
-                ))} */}
-
-                {/* {Math.max(0, currentPage - Math.floor(visiblePages / 2))}
-                -
-                {Math.min(totalPages, pageStart + visiblePages)}
-                -
-                {displayedPages}
-                -
-                {pageEnd } */}
+                { resp == "pc" && (
+                    <PaginationItem className={currentPage != 1 ? 'cursor-pointer' : 'cursor-default'}>
+                        <PaginationPrevious onClick={handlePrevPage} className={currentPage === 1 ? "bg-neutral-200 rounded-md" : ""} />
+                    </PaginationItem>
+                )}
 
                 {/* pages are far from the start */}
                 {pageStart > 0 && (
                     <>
                         <PaginationItem className={currentPage != 1 ? 'cursor-pointer' : 'cursor-default'}>
-                            <PaginationLink onClick={() => setCurrentPage(1)}>1</PaginationLink>
+                            <PaginationLink className={buttonSize} onClick={() => setCurrentPage(1)}>1</PaginationLink>
                         </PaginationItem>
                         {pageStart > 1 && <PaginationItem><PaginationEllipsis /></PaginationItem>}
                     </>
@@ -114,14 +101,13 @@ const PaginationSection: React.FC<PaginationProps> = ({
                         {
                             currentPage === page ? 
                             <>
-                                <PaginationLink isActive onClick={() => setCurrentPage(page)}>{page}</PaginationLink>
+                                <PaginationLink className={buttonSize} isActive onClick={() => setCurrentPage(page)}>{page}</PaginationLink>
                             </>
                             :
                             <>
-                                <PaginationLink onClick={() => setCurrentPage(page)}>{page}</PaginationLink>
+                                <PaginationLink className={buttonSize} onClick={() => setCurrentPage(page)}>{page}</PaginationLink>
                             </>
                         }
-                        
                     </PaginationItem>
                 ))}
 
@@ -130,14 +116,17 @@ const PaginationSection: React.FC<PaginationProps> = ({
                     <>
                         {pageEnd < totalPages && <PaginationItem><PaginationEllipsis /></PaginationItem>}
                         <PaginationItem className={currentPage != totalPages ? 'cursor-pointer' : 'cursor-default'}>
-                            <PaginationLink onClick={() => setCurrentPage(totalPages)}>{totalPages}</PaginationLink>
+                            <PaginationLink className={buttonSize} onClick={() => setCurrentPage(totalPages)}>{totalPages}</PaginationLink>
                         </PaginationItem>
                     </>
                 )}
 
-                <PaginationItem className={currentPage != totalPages ? 'cursor-pointer' : 'cursor-default'}>
-                    <PaginationNext title={next} onClick={handleNextPage} className={currentPage === totalPages ? "bg-neutral-200 rounded-md" : ""} />
+                {resp == "pc" && (
+                <PaginationItem className={currentPage !== totalPages ? 'cursor-pointer' : 'cursor-default'}>
+                    <PaginationNext onClick={handleNextPage} className={currentPage === totalPages ? "bg-neutral-200 rounded-md" : ""} />
                 </PaginationItem>
+                )}
+                
             </PaginationContent>
         </Pagination>
     );
